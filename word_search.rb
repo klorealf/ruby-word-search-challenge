7a31a6b04abfb6c7c38d9ae67ba158966d161992
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 12:28:24 -0400

    Work on feature for looking at array columns

--- word_search.rb
+++ word_search.rb
@@ -1,5 +1,5 @@
 def straight_line_include?(word, puzzle)
-  # word_present = nil
+
   str_puzzle_rows = ""
   puzzle_rows = puzzle.length
   puzzle_columns = puzzle[0].length
@@ -8,6 +8,13 @@ def straight_line_include?(word, puzzle)
     str_puzzle_rows += puzzle_iteration.join("")
     if str_puzzle_rows.include?(word)
       return true
+
+    end
+    row_counter = 0
+    nested_arrays.each do |el|
+      # print string with letter index and letter
+      p "#{row_counter} - #{el}"
+      row_counter += 1
     end
   end
   return false
