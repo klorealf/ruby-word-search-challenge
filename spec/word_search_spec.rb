6ea4852ea0e7141bf93cd1092e0d3345fefc7e3d
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 10:09:59 -0400

    Add rspec test for print nested array

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -1,6 +1,25 @@
 require_relative '../word_search'
 
 describe 'straight_line_include?' do
+  let(:puzzle) {[
+  ["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]] }
+
+  it "iterates through and prints a nested array" do
+    nested = straight_line_include?("", puzzle)
+    expect(nested).to eq [["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]]
+  end
 end
 
 describe 'snaking_include?' do
