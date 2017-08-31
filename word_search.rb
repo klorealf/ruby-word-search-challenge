19b436d68ca176ff5c56744fe062ddf106798d9d
Samory Harris <email": null>
2017-08-31 18:33:53 -0400

    Add puzzle data structure to release 0

--- word_search.rb
+++ word_search.rb
@@ -1,5 +1,31 @@
+puzzle = [
+  ["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+]
+
+# p puzzle[0][1..6].join
+
 def straight_line_include?(word, puzzle)
+  puzzle.each do |letters|
+    if puzzle[0][1..6].join == "wolves"
+      return true
+    end
+  end
+
+  puzzle.each do |letters|
+    if puzzle[1][2].join == "otters"
+      return true
+    end
+  end
 end
+# p straight_line_include?("wolves", puzzle)
+
+
 
 def snaking_include?(word, puzzle)
 end
