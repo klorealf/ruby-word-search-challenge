dae78cc1b1ce61eb3ff9b0a7eb9ae16bd3f009cb
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 11:44:52 -0400

    Add rspec test

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -10,15 +10,14 @@ describe 'straight_line_include?' do
   ["u", "r", "p", "i", "w", "e", "u"],
   ["z", "s", "b", "n", "u", "i", "r"]] }
 
-  it "iterates through and prints a nested array" do
-    nested = straight_line_include?("", puzzle)
-    expect(nested).to eq [["a", "w", "o", "l", "v", "e", "s"],
-  ["s", "o", "a", "w", "a", "h", "p"],
-  ["i", "t", "c", "k", "e", "t", "n"],
-  ["o", "t", "s", "d", "h", "o", "h"],
-  ["s", "e", "h", "g", "s", "t", "a"],
-  ["u", "r", "p", "i", "w", "e", "u"],
-  ["z", "s", "b", "n", "u", "i", "r"]]
+  it "returns true if the word is present in a row" do
+    search_rows = straight_line_include?("wolves", puzzle)
+    expect(search_rows).to eq true
+  end
+
+  it "returns false if the word is not present in a row" do
+    search_rows = straight_line_include?("dancing", puzzle)
+    expect(search_rows).to eq false
   end
 end
 
