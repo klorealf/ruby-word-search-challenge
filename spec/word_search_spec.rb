de5be1f8f182fba5b1179f04774f664d6422228a
Catherine Rice <clr2107@gmail.com>
2017-08-31 12:26:57 -0400

    Add straight line word search method

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -1,6 +1,14 @@
 require_relative '../word_search'
 
 describe 'straight_line_include?' do
+
+  it "returns true if a method is present in the puzzle" do
+    expect(straight_line_include?("otter", puzzle).to eq(true)
+  end
+  it "returns false if a method is not present in the puzzle" do
+    expect(straight_line_include?("banana"), puzzle).to eq(false)
+  end
+
 end
 
 describe 'snaking_include?' do
