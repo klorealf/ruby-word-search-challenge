e87f09070da06c57025dcae031a5253d494986a1
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 11:47:40 -0400

    modify variable name

--- word_search.rb
+++ word_search.rb
@@ -1,14 +1,14 @@
 def straight_line_include?(word, puzzle)
-  result = nil
+  word_present = nil
   str_puzzle_rows = ""
   puzzle_rows = puzzle.length
   puzzle_columns = puzzle[0].length
   puzzle.each do |nested_arrays|
     puzzle_iteration = nested_arrays
     str_puzzle_rows += puzzle_iteration.join("")
-    result = str_puzzle_rows.include?(word)
+    word_present = str_puzzle_rows.include?(word)
   end
-  p result
+  word_present
 end
 
 def snaking_include?(word, puzzle)
