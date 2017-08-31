97d8415dfbb2ea2ca42dcc166097fef6a3914475
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 09:55:59 -0400

    Remove un-needed nested array and method call

--- word_search.rb
+++ word_search.rb
@@ -1,13 +1,3 @@
-puzzle = [
-  ["a", "w", "o", "l", "v", "e", "s"],
-  ["s", "o", "a", "w", "a", "h", "p"],
-  ["i", "t", "c", "k", "e", "t", "n"],
-  ["o", "t", "s", "d", "h", "o", "h"],
-  ["s", "e", "h", "g", "s", "t", "a"],
-  ["u", "r", "p", "i", "w", "e", "u"],
-  ["z", "s", "b", "n", "u", "i", "r"]
-]
-
 def straight_line_include?(word, puzzle)
   puzzle.each do |nested_array|
     nested_array.each do |search_letter|
@@ -15,8 +5,6 @@ def straight_line_include?(word, puzzle)
     end
   end
 end
-
-straight_line_include?("wolves", puzzle)
 
 # def snaking_include?(word, puzzle)
 # end
