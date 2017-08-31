e69262b95d10407f69a127cb96b4981df4bfec83
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 11:56:52 -0400

    Refactor method conditionals

--- word_search.rb
+++ word_search.rb
@@ -1,14 +1,16 @@
 def straight_line_include?(word, puzzle)
-  word_present = nil
+  # word_present = nil
   str_puzzle_rows = ""
   puzzle_rows = puzzle.length
   puzzle_columns = puzzle[0].length
   puzzle.each do |nested_arrays|
     puzzle_iteration = nested_arrays
     str_puzzle_rows += puzzle_iteration.join("")
-    word_present = str_puzzle_rows.include?(word)
+    if str_puzzle_rows.include?(word)
+      return true
+    end
   end
-  word_present
+  return false
 end
 
 def snaking_include?(word, puzzle)
