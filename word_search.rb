3ad3d7039d72d4f8dafce75c7641ca5b97ec29e4
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 09:54:02 -0400

    Add iteration to method

--- word_search.rb
+++ word_search.rb
@@ -1,5 +1,22 @@
+puzzle = [
+  ["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+]
+
 def straight_line_include?(word, puzzle)
+  puzzle.each do |nested_array|
+    nested_array.each do |search_letter|
+      p search_letters
+    end
+  end
 end
 
-def snaking_include?(word, puzzle)
-end
+straight_line_include?("wolves", puzzle)
+
+# def snaking_include?(word, puzzle)
+# end
