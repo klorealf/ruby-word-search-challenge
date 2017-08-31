2c53dd51dd14ab8a2bfa28d7864f5636a8c38846
klorealf <email": null>
2017-08-31 18:01:43 -0400

    add pseudo code

--- word_search.rb
+++ word_search.rb
@@ -1,4 +1,15 @@
+#accept two arguments:
+# (1) a word we're looking for and
+# (2) a word search puzzle modeled as a nested array.
+# The method will return true if the word can be found in the puzzle
+# and false if not.
+# only allow words to be found in straight lines
+#Words can be found in horizontal, vertical, and diagonal lines.
+#words can be spelled forward and backward.
+
 def straight_line_include?(word, puzzle)
+
+
 end
 
 def snaking_include?(word, puzzle)
