a6f8252c946bc85a7a2525438deeddce6875d79c
aarias89 <andresar11@gmail.com>
2017-09-01 08:00:28 -0400

    Add spec tests for word_search horizontal, diagonal and vertical

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -1,7 +1,49 @@
 require_relative '../word_search'
 
 describe 'straight_line_include?' do
+  let(:puzzle) { [
+    ["a", "w", "o", "l", "v", "e", "s"],
+    ["s", "o", "a", "w", "a", "h", "p"],
+    ["i", "t", "c", "k", "e", "t", "n"],
+    ["o", "t", "s", "d", "h", "o", "h"],
+    ["s", "e", "h", "g", "s", "t", "a"],
+    ["u", "r", "p", "i", "w", "e", "u"],
+    ["z", "s", "b", "n", "u", "i", "r"]
+  ] }
+  describe 'check_horizontal?' do
+    it 'checks word' do
+      expect(straight_line_include?('wolves', puzzle)).to be true
+    end
+    it 'checks word backward' do
+      expect(straight_line_include?('ehgs', puzzle)).to be true
+    end
+  end
+
+  describe 'check_vertical?' do
+    it 'checks word' do
+      expect(straight_line_include?('otters', puzzle)).to be true
+    end
+    it 'checks word backward' do
+      expect(straight_line_include?('lwkdg', puzzle)).to be true
+    end
+  end
+
+  describe 'check_diagonal?' do
+    it 'checks word decending forward' do
+      expect(straight_line_include?('dser', puzzle)).to be true
+    end
+    it 'checks word decending backward' do
+      expect(straight_line_include?('dhrz', puzzle)).to be true
+    end
+    it 'checks word ascending forward' do
+      expect(straight_line_include?('bison', puzzle)).to be true
+    end
+    it 'checks word ascending backward' do
+      expect(straight_line_include?('dcoa', puzzle)).to be true
+    end
+  end
 end
 
 describe 'snaking_include?' do
+
 end
