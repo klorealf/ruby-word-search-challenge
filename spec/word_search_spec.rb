497fd8ea57e9a91e38fa126e6e0bd9b126990d2c
Andreas Landgrebe <andreaslandgrebe@gmail.com>
2017-09-04 18:21:56 -0400

    finish release 0. Wrote method to find word in straight line and wrote tests to find words in puzzle

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -1,6 +1,24 @@
 require_relative '../word_search'
 
 describe 'straight_line_include?' do
+	puzzle = [
+	["a", "w", "o", "l", "v", "e", "s"], 
+	["s", "o", "a", "w", "a", "h", "p"], 
+	["i", "t", "c", "k", "e", "t", "n"],
+	["o", "t", "s", "d", "h", "o", "h"],
+	["s", "e", "h", "g", "s", "t", "a"],
+	["u", "r", "p", "i", "w", "e", "u"],
+	["z", "s", "b", "n", "u", "i", "r"]
+]
+	it "checks to see if wolves is in puzzle" do
+		expect(straight_line_include?('wolves', puzzle)).to eq true
+	end
+	it "checks to see if otters is in puzzle" do
+		expect(straight_line_include?('otters', puzzle)).to eq true
+	end
+	it "checks to see if bison is in puzzle" do
+		expect(straight_line_include?('bison', puzzle)).to eq true
+	end
 end
 
 describe 'snaking_include?' do
--- word_search.rb
+++ word_search.rb
@@ -1,4 +1,121 @@
+puzzle = [
+	["a", "w", "o", "l", "v", "e", "s"], 
+	["s", "o", "a", "w", "a", "h", "p"], 
+	["i", "t", "c", "k", "e", "t", "n"],
+	["o", "t", "s", "d", "h", "o", "h"],
+	["s", "e", "h", "g", "s", "t", "a"],
+	["u", "r", "p", "i", "w", "e", "u"],
+	["z", "s", "b", "n", "u", "i", "r"]
+]
+#require 'pry'
 def straight_line_include?(word, puzzle)
+	#binding.pry
+
+	word_array = word.split(//)
+	#word_array_length = word_array.length
+
+	#forward horizontal (east)
+	for row in 0..(puzzle.length - 1)
+		for col in 0..(puzzle[row].length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row][col + index] == word_array[index])
+				index += 1
+			end
+			if (puzzle[row][col + index]) == (word_array[index])
+				return true
+			end
+		end
+	end
+	#horizontal backwards (west)
+	for row in 0..(puzzle.length - 1)
+		for col in 0..(puzzle[row].length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row][col + index] == word_array[word.length - 1 - index])
+				index += 1
+			end
+			if (puzzle[row][col + index]) == (word_array[word.length - 1 - index])
+				return true
+			end
+		end
+	end
+	# downward vertical (south)
+	for col in 0..(puzzle[0].length - 1)
+		for row in 0..(puzzle.length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row + index][col] == word_array[index])
+				index += 1
+			end
+			if puzzle[row + index][col] == word_array[index]
+				return true
+			end
+		end
+	end
+	#upward vertical(north)
+	for col in 0..(puzzle[0].length - 1)
+		for row in 0..(puzzle.length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row + index][col] == word_array[word.length - 1 - index])
+				index += 1
+			end
+			if puzzle[row + index][col] == word_array[word.length - 1 - index]
+				return true
+			end
+		end
+	end
+	#diagonally downard (southeast)
+	for row in 0..(puzzle.length - word.length)
+		for col in 0..(puzzle[row].length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row + index][col + index] == word_array[index])
+				index += 1
+			end
+			if puzzle[row + index][col + index] == word_array[index]
+				return true
+			end
+		end
+	end
+
+
+	#diagonally upward(northwest)	
+	for row in 0..(puzzle.length - word.length)
+		for col in 0..(puzzle[row].length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row + index][col + index] == word_array[word.length - 1 - index])
+				index += 1
+			end
+			if puzzle[row + index][col + index] == word_array[word.length - 1 - index]
+				return true
+			end
+		end
+	end
+	#diagonally downward(southwest)
+	#incorrect(for right now)
+	for row in 0..(puzzle.length - word.length)
+		for col in (word.length - 1)..(puzzle[row].length - 1)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row + index][col - index] == word_array[index])
+				index += 1
+			end
+			if puzzle[row + index][col - index] == word_array[index]
+				return true
+			end
+		end
+	end
+
+	#diagonally upward(northeast)
+	for row in (word.length - 1)..(puzzle.length - 1)
+		for col in 0..(puzzle[0].length - word.length)
+			index = 0
+			while (index < word.length - 1) && (puzzle[row - index][col + index] == word_array[index])
+				index += 1
+			end
+			if puzzle[row - index][col + index] == word_array[index]
+				return true
+			end
+		end
+	end
+
+	return false
 end
 
 def snaking_include?(word, puzzle)
