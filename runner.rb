abd4a3ff41ea301efac605df9dd8f74308d5083a
Thierry Moudiki <email": null>
2017-09-08 10:53:10 -0400

    refactor - delete for loops

--- runner.rb
+++ runner.rb
@@ -1,1 +1,23 @@
 require_relative 'word_search'
+
+puzzle = [
+  ["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+]
+
+puts "puzzle"
+p puzzle
+puts "\n"
+puts "search 'bison'"
+p straight_line_include?("bison", puzzle)
+puts "search 'otters'"
+p straight_line_include?("otters", puzzle)
+puts "search 'wolves'"
+p straight_line_include?("wolves", puzzle)
+puts "search 'orpekw'"
+p straight_line_include?("orpekw", puzzle)
--- word_search.rb
+++ word_search.rb
@@ -18,76 +18,38 @@ step3: loop through diagonals
 output: a boolean = true if the word is found, and false otherwise
 =end
 
-#https://repl.it/KaL8/32
-
+# search in rows
 def search_rows(row_index, word, puzzle)
-nb_columns = puzzle[0].length
-
-current_word = ""
-
-  for j in 0..nb_columns-1
-    current_word << puzzle[row_index][j]
-  end
-
-  current_word.include?(word) || current_word.include?(word.reverse)
-
+  nb_columns = puzzle[0].length
+  current_word = puzzle[row_index].map{|elm| elm}.join('')
+  return current_word.include?(word) || current_word.include?(word.reverse)
 end
 
+# search in columns
 def search_cols(col_index, word, puzzle)
-nb_rows = puzzle.length
-
-current_word = ""
-
- for i in 0..nb_rows-1
-   current_word << puzzle[i][col_index]
- end
-
-  current_word.include?(word) || current_word.include?(word.reverse)
-
+  nb_rows = puzzle.length
+  col_word = (0..nb_rows-1).map{|elm| puzzle[elm][col_index]}
+  current_word = col_word.map{|elm| elm}.join('')
+  return current_word.include?(word) || current_word.include?(word.reverse)
 end
 
+# search in diags part 1
 def search_diags(diag_num, word, puzzle)
-nb_columns = puzzle[0].length
-nb_rows = puzzle.length
-nb_diags = nb_rows*2 - 1
-
-current_word = ""
-elt = ""
+  nb_columns = puzzle[0].length
+  nb_rows = puzzle.length
+  nb_diags = nb_rows*2 - 1
 
   if diag_num < nb_columns
-    for i in 0..diag_num
-        # works until diag_num == nb_columns - 1
-          current_word << puzzle[i][diag_num-i]
-    end
-  else
-    for i in 1..2*nb_columns-(diag_num+1)
-      elt = puzzle[diag_num-nb_columns + i][nb_columns-i]
-      #p elt
-      #if elt != nil
-        current_word << elt
-      #end
-    end
+     current_word = (0..diag_num).map{|elm| puzzle[elm][diag_num-elm]}.join('')
+   else
+     current_word = (1..2*nb_columns-(diag_num+1)).map{|elm| puzzle[diag_num-nb_columns + elm][nb_columns-elm]}.join('')
   end
 
-  current_word.include?(word) || current_word.include?(word.reverse)
-
+  return current_word.include?(word) || current_word.include?(word.reverse)
 end
-
-puzzle = [
-  ["a", "w", "o", "l", "v", "e", "s"],
-  ["s", "o", "a", "w", "a", "h", "p"],
-  ["i", "t", "c", "k", "e", "t", "n"],
-  ["o", "t", "s", "d", "h", "o", "h"],
-  ["s", "e", "h", "g", "s", "t", "a"],
-  ["u", "r", "p", "i", "w", "e", "u"],
-  ["z", "s", "b", "n", "u", "i", "r"]
-]
-
-
 
 
 def straight_line_include?(word, puzzle)
-
   # index for rows and columns
   row_index = 0
   col_index = 0
@@ -96,7 +58,6 @@ def straight_line_include?(word, puzzle)
   nb_rows = puzzle.length
   nb_columns = puzzle[0].length
   nb_diags = nb_rows*2 - 1
-  res = []
 
   # step1: loop through lines
   res_rows = (0..nb_rows-1).map{|row_index| search_rows(row_index, word, puzzle)}
@@ -109,11 +70,6 @@ def straight_line_include?(word, puzzle)
 
   return res_rows.any?{|elm| elm == true} || res_columns.any?{|elm| elm == true} || res_diags.any?{|elm| elm == true}
 end
-
-p straight_line_include?("bison", puzzle)
-p straight_line_include?("otters", puzzle)
-p straight_line_include?("wolves", puzzle)
-p straight_line_include?("orpekw", puzzle)
 
 def snaking_include?(word, puzzle)
 
